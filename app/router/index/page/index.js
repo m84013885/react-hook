08b0c39d33e79cad30bd4a9ead1c14b0497dd022
m84013885 <84013885@qq.com>
2019-08-13 14:49:37 +0800

    fix

--- app/router/index/page/index.js
+++ app/router/index/page/index.js
@@ -1,19 +1,36 @@
 import style from './index.css'
 import axios from 'axios'
 import md5 from 'md5'
+import ScrollView from './scroll'
 
 const Main = () => {
-  axios.post('https://huangqm.xyz/php/sig.php', {
-    sig: md5('ming' + 'test' + 'www'),
-    s: 'www',
-    d: 'test'
-  })
-    .then(function (res) {
-      console.log(res)
-    })
-    .catch(function (error) {
-      console.log(error)
-    })
-  return <div className={style.test}>按钮</div>
+  // axios.post('https://huangqm.xyz/php/sig.php', {
+  //   sig: md5('ming' + 'test' + 'www'),
+  //   s: 'www',
+  //   d: 'test'
+  // })
+  //   .then(function (res) {
+  //     console.log(res)
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error)
+  //   })
+  return (
+    <ScrollView>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>3</div>
+      <div>3</div>
+      <div>3</div>
+      <div>3</div>
+      <div>3</div>
+      <div>3</div>
+      <div>3</div>
+      <div>3</div>
+      <div>3</div>
+      <div>3</div>
+    </ScrollView>
+  )
 }
 export default Main
---
+++ app/router/index/page/scroll/index.js
@@ -0,0 +1,41 @@
+
+import { useState, useEffect, useRef } from "react"
+import BScroll from "better-scroll"
+import style from './index.css'
+
+const ScrollView = ((props) => {
+    const [bScroll, setBScroll] = useState()
+    const scrollContaninerRef = useRef(null)
+
+    useEffect(() => {
+        if (bScroll) return
+        const scroll = new BScroll(scrollContaninerRef.current, {
+            scrollX: false,
+            scrollY: true,
+            bounce: {
+                top: true,
+                bottom: true
+            }
+        })
+        scroll.on('scrollEnd', () => {
+            //判断是否滑动到了底部
+            if (scroll.y <= scroll.maxScrollY + 100) {
+                console.log('底部')
+            }
+        })
+        setBScroll(scroll)
+        return () => {
+            scroll.off('scroll')
+            setBScroll(null)
+        }
+    }, [])
+    return (
+        <div className={style.scrollStyle} ref={scrollContaninerRef}>
+            <div>
+                {props.children}
+            </div>
+        </div>
+    )
+})
+
+export default ScrollView
