5f9fc8b180576b9523cd1c94a2c100d1405eb7ab
m84013885 <84013885@qq.com>
2019-09-18 16:41:39 +0800

    fix : useReducer的方法

--- app/router/example/page/index.js
+++ app/router/example/page/index.js
@@ -1,14 +1,27 @@
 import style from './index.css'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useReducer } from 'react'
+
+const testFun = (num, param) => {
+  console.log(num)
+  console.log(param)
+  return num + param
+}
 
 const Main = () => {
   // useState的简单使用
   const [count, setCount] = useState(0)
+
+  const [num, fun] = useReducer(testFun, 0)
   // useEffect的特殊用法（只执行一次）
   useEffect(() => {
   }, [])
-  return <div className={style.test} onClick={() => { setCount(1 + count) }}>{count}</div>
+  return (
+    <React.Fragment>
+      <div className={style.test} onClick={() => { setCount(1 + count) }}>{count}</div>
+      <div onClick={() => { fun(5) }}>{num}</div>
+    </React.Fragment>
+  )
 }
 
 export default Main
