99f0829ae0bf113999a2a6ebff1bdeb31c927a57
m84013885 <84013885@qq.com>
2019-11-12 19:24:55 +0800

    fix

--- app/router/index/page/index.js
+++ app/router/index/page/index.js
@@ -1,12 +1,21 @@
 import style from './index.css'
 import axios from 'axios'
 import md5 from 'md5'
-import React from 'react'
+import React, { useState, useEffect,useMemo, useCallback } from 'react'
 import ScrollView from './scroll'
 import title from '../../../images/title.common.png'
 import Toast from './toast'
 
 const Main = () => {
+  const [memoValue, setMemoValue] = useState(1)
+  const onceDom = useMemo(() => {
+    return <div>{memoValue}</div>
+  }, [])
+  const increment1 = useCallback(() => setMemoValue(c => c + 1), [])
+
+  const onceDom1 = useCallback(() => {
+    return 1
+  }, [])
   // axios.post('https://huangqm.xyz/php/sig.php', {
   //   sig: md5('ming' + 'test' + 'www'),
   //   s: 'www',
@@ -20,8 +29,10 @@ const Main = () => {
   //   })
   return (
     <ScrollView>
-      <div className={style.test} style={{backgroundImage:`url(${title})`}}></div>
-      <div className={style.test1}></div>
+      {onceDom}
+      <div>{memoValue}</div>
+      <div className={style.test} style={{ backgroundImage: `url(${title})` }} onClick={increment1}></div>
+      <div className={style.test1} onClick={}></div>
       <div>1</div>
       <div>2</div>
       <div>3</div>
@@ -35,7 +46,7 @@ const Main = () => {
       <div>3</div>
       <div>3</div>
       <div>3</div>
-      <Toast/>
+      <Toast />
     </ScrollView>
   )
 }
