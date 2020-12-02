be547107c421ac0d3a93dd1b72a8798382ce46d1
m84013885 <84013885@qq.com>
2020-12-02 16:12:39 +0800

    fix

--- app/router/index/index.js
+++ app/router/index/index.js
@@ -5,6 +5,21 @@ import { render } from 'react-dom'
 import VConsole from 'vconsole'
 const v = new VConsole()
 
+const dpr = window.devicePixelRatio || 1
+const docEl = document.documentElement
+// detect 0.5px supports
+if (dpr >= 2) {
+    const fakeBody = document.createElement('body')
+    const testElement = document.createElement('div')
+    testElement.style.border = '.5px solid transparent'
+    fakeBody.appendChild(testElement)
+    docEl.appendChild(fakeBody)
+    if (testElement.offsetHeight === 1) {
+        docEl.classList.add('hairlines')
+    }
+    docEl.removeChild(fakeBody)
+}
+
 import './index.css'
 import Main from './page'
 
