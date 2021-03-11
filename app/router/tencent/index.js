fdf7277037465c36e893347c90c738031ffa415b
m84013885 <84013885@qq.com>
2021-03-11 11:28:11 +0800

    fix

--- app/router/tencent/index.js
+++ app/router/tencent/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './index.css'
 import { render } from 'react-dom'
 import Main from './page'
+import '../../utils/resize'
+
 
 window.onload = function () {
   render(<Main />, document.getElementById('main'))
--- app/router/tencent/page/index.js
+++ app/router/tencent/page/index.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react'
-import ejs from 'ejs'
-
-console.log(html)
+import style from './index.css'
 
 const Main = () => {
   return (
-    <div>123</div>
+    <div className={style.main}>
+      <div>
+        1
+        <div className={style.item}></div>
+      </div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      {/* <header>1</header>
+      <aside>4</aside>
+      <nav>2</nav>
+      <main>5</main> */}
+      {/* <footer>3</footer> */}
+    </div>
   )
 
 }
